893aaa890547358cd336cd969f3eb628e6127482
Michael Romero <romerom@gmail.com>
2017-11-27 01:02:07 -0800

    got user registration working to firebase - next will be to move forms into modal

--- app/controllers/users/new.js
+++ app/controllers/users/new.js
@@ -1,28 +1,37 @@
 import Controller from '@ember/controller';
+import Ember from 'ember';
 
 export default Controller.extend({
+  firebaseApp: Ember.inject.service(),
   actions: {
-    create() {
-      var newUser = this.store.createRecord('user', {
-        username: this.get('model.user.username'), // we use model.user since we're using an RSVPHash
-        pw_hash: this.get('model.user.username'), // this will need to change, obviously.
-        email: this.get('model.user.email'),
-        firstName: this.get('model.user.firstName'),
-        lastName: this.get('model.user.lastName'),
-        bio: this.get('model.user.bio'),
-        memberSince: new Date(),
-        post_score: 0,
-        comment_score: 0,
-        posts: [], // does not end up in data..
-        favorite_posts: [],
-        disliked_posts: [],
-        favorite_comments: [],
-        disliked_comments: [],
-        comments: []
-      });
-      var self = this;
-      newUser.save().then(function() {
-        self.transitionToRoute('users');
+    create(email, pass) {
+      // https://firebase.google.com/docs/reference/js/firebase.auth.Auth#createUserWithEmailAndPassword
+      const auth = this.get('firebaseApp').auth();
+      auth.createUserWithEmailAndPassword(email, pass).then((userResponse) => {
+        const user = this.store.createRecord('user', {
+          id: userResponse.uid,
+          email: userResponse.email, // must be unique
+          username: this.get('model.user.username'),
+          firstName: this.get('model.user.firstName'),
+          lastName: this.get('model.user.lastName'),
+          bio: this.get('model.user.bio'),
+          memberSince: new Date(),
+          post_score: 0,
+          comment_score: 0
+        });
+        var self = this;
+        return user.save().then(function() {
+          self.get('session').open('firebase', {
+            provider: 'password',
+            email: email,
+            password: pass
+          });
+          self.transitionToRoute('users');
+        });
+      }).catch(function(error) {
+        //var errorCode = error.code; //
+        var errorMessage = error.message;
+        alert(errorMessage);
       });
     },
     cancel() {
--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,19 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  username: DS.attr('string'), // how can we ensure this is unique?
-  pw_hash: DS.attr('string'), // unsure here..
   email: DS.attr('string'),
+  password: DS.attr('string'), // we never actually save this... just for form manipulation
+  username: DS.attr('string'), // how can we ensure this is unique?
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   bio: DS.attr('string'),
   memberSince: DS.attr('date'),
-  post_score: DS.attr('number'), // calculated value of each posts score
-  comment_score: DS.attr('number'), // calculated value of each comment score
+  post_score: DS.attr('number'), // TODO: calculated value of each posts score
+  comment_score: DS.attr('number'), // TODO: calculated value of each comment score
   posts: DS.hasMany('post'), //  needs to be an array of post id's...
+  comments: DS.hasMany('comment'), // should be an array of comment ids
   favorite_posts: DS.hasMany('post', {inverse: 'upvoters'}), // should be an array of post id's..
   disliked_posts: DS.hasMany('post', {inverse: 'downvoters'}), // should be an array of post id's..
   favorite_comments: DS.hasMany('comment', {inverse: 'upvoters'}),
-  disliked_comments: DS.hasMany('comment', {inverse: 'downvoters'}),
-  comments: DS.hasMany('comment') // should be an array of comment ids
+  disliked_comments: DS.hasMany('comment', {inverse: 'downvoters'})
 });
