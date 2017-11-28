2fba46c53137df2a29b209bf2949dd9435fcc9a6
Michael Romero <romerom@gmail.com>
2017-11-28 13:29:50 -0800

    cleaned some things up

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+    // this really should return values in reverse... right now it doesn't.
     return this.store.findAll('post');
   }
 });
