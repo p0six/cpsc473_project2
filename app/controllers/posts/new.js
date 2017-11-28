41ff4de32b0c0be2620425168e98bd0c4a7271dc
Michael Romero <romerom@gmail.com>
2017-11-27 21:31:03 -0800

    derp

--- app/controllers/posts/new.js
+++ app/controllers/posts/new.js
@@ -8,33 +8,33 @@ export default Controller.extend({
   progress: 0,
   actions: {
     didSelectFiles(data) {
-      console.log(data);
+      //console.log(data);
       const storageRef = window.firebase.storage().ref();
       let file = data;
       var uploadTask = storageRef.child('images/' + file[0].name).put(file[0]);
-      uploadTask.on(window.firebase.storage.TaskEvent.STATE_CHANGED,
-        (snapshot) => {
-          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-          this.set('progressText', `Upload is ${Math.round(progress * 100) / 100} % done`);
-          this.set('progress', progress);
-          switch (snapshot.state) {
-            case window.firebase.storage.TaskState.PAUSED:
-              this.set('status', 'Upload is paused');
-              break;
-            case window.firebase.storage.TaskState.RUNNING:
-              this.set('status', 'Upload is running');
-              break;
-          }
-        }, (error) => {
-        switch (error.code) {
+      uploadTask.on(window.firebase.storage.TaskEvent.STATE_CHANGED, (snapshot) => {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //console.log('Upload is ' + progress + '% done');
+        this.set('progressText', `Upload is ${Math.round(progress * 100) / 100} % done`);
+        this.set('progress', progress);
+        switch (snapshot.state) {
+          case window.firebase.storage.TaskState.PAUSED:
+            this.set('status', 'Upload is paused');
+            break;
+          case window.firebase.storage.TaskState.RUNNING:
+            this.set('status', 'Upload is running');
+            break;
+        }
+      }, (error) => {
+        alert(error.message);
+        /*switch (error.code) {
           case 'storage/unauthorized':
             break;
           case 'storage/canceled':
             break;
           case 'storage/unknown':
             break;
-        }
+        }*/
       }, () => {
         this.set('downloadURL', uploadTask.snapshot.downloadURL);
       });
