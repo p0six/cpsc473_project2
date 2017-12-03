b74bc3c8edefef95a8f61abc3100fb8c5fc66544
Michael Romero <romerom@gmail.com>
2017-12-03 00:16:01 -0800

    replaced code causing deprecation warning related to handlebars and styles with a call to a new helper that produces an htmlSafe string

---
+++ app/helpers/index-image-background.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+import Ember from 'ember';
+
+export function indexImageBackground(params/*, hash*/) {
+  return Ember.String.htmlSafe("background-image: url(" + params[0] + ')')
+}
+
+export default helper(indexImageBackground);
