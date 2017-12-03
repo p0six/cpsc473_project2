af93536a8e22d3fc7d253a2c9e634033551eeb1e
unknown <CampusUser@CSUFLAPTOP-97.WORKGROUP>
2017-12-02 17:47:37 -0800

    SweetAlert in Edit Profile

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,9 +3,13 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   //user: DS.belongsTo('user'),
   //post: DS.belongsTo('post'),
-  comment: DS.attr('string'),
-  dateSubmitted: DS.attr('date'),
-  score: DS.attr('number')
+
+
+  // comment: DS.attr('string'),
+  // dateSubmitted: DS.attr('date'),
+  // score: DS.attr('number')
+
+  
   //votes : Ember.computed('score', function(){});
   //,
   //upvoters: DS.hasMany('user', {inverse: 'favorite_comments'}),
--- app/models/user.js
+++ app/models/user.js
@@ -5,14 +5,14 @@ export default DS.Model.extend({
   displayName: DS.attr('string'), // how can we ensure this is unique?
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  bio: DS.attr('string'),
-  memberSince: DS.attr('date'),
-  post_score: DS.attr('number'), // TODO: calculated value of each posts score
-  comment_score: DS.attr('number'), // TODO: calculated value of each comment score
-  posts: DS.hasMany('post'), //  needs to be an array of post id's...
-  comments: DS.hasMany('comment'), // should be an array of comment ids
-  favorite_posts: DS.hasMany('post', {inverse: 'upvoters'}), // should be an array of post id's..
-  disliked_posts: DS.hasMany('post', {inverse: 'downvoters'}), // should be an array of post id's..
-  favorite_comments: DS.hasMany('comment', {inverse: 'upvoters'}),
-  disliked_comments: DS.hasMany('comment', {inverse: 'downvoters'})
+  bio: DS.attr('string')
+  // memberSince: DS.attr('date'),
+  // post_score: DS.attr('number'), // TODO: calculated value of each posts score
+  // comment_score: DS.attr('number'), // TODO: calculated value of each comment score
+  // posts: DS.hasMany('post'), //  needs to be an array of post id's...
+  // comments: DS.hasMany('comment'), // should be an array of comment ids
+  // favorite_posts: DS.hasMany('post', {inverse: 'upvoters'}), // should be an array of post id's..
+  // disliked_posts: DS.hasMany('post', {inverse: 'downvoters'}), // should be an array of post id's..
+  // favorite_comments: DS.hasMany('comment', {inverse: 'upvoters'}),
+  // disliked_comments: DS.hasMany('comment', {inverse: 'downvoters'})
 });
