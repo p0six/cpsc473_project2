e14ed118c4a0afe8d9782f639f3e23c2df474350
julianv6 <22julianv@gmail.com>
2017-12-03 13:18:31 -0800

    fixes the link-to query param problems

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,6 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  queryParams:{
+    myIndex:{
+      refreshModel: true,
+      replace: true
+    },
+    posts:{
+      refreshModel: true,
+      replace: true
+    }
+  },
   model(params){
       return this.store.findRecord('post', params.post_id);
   }
