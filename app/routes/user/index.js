bcd9dbf8265f067e80f7f8c89ff36e9fef432677
julianv6 <22julianv@gmail.com>
2017-12-03 18:38:53 -0800

    view profile page on its way to being complete (this will lead to formating individual other users' page), made gallery-view component so we can use it throught. thinking about doing the same for comments

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return this.store.query('post', {
+      user: this.get('session.currentUser.uid')/*,
+      limitToLast: 15 */
+    });
+  }
 });
