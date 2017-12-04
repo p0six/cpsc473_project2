5da23139dc1e24a8767b418565ef9e64f4f547af
unknown <CampusUser@CSUFLAPTOP-97.WORKGROUP>
2017-12-03 21:28:58 -0800

    Commenting TODO for referential integrity

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -52,7 +52,26 @@ export default Controller.extend({
       console.log(this.get('posts').objectAt(this.get('nextIndex')));*/
     },
     postComment(postid,username) {
+
+      // TODO: correct referential integrity - user
+
       var self = this;
+      this.store.findRecord('post', postid).then(function(post) {
+        const comment = self.store.createRecord('comment', {
+          comment: self.get('newcomment'),
+          post: post,
+          dateSubmitted: new Date(),
+          user: username
+        });
+          comment.save().then(function(myComment) {
+            self.set('newcomment', '');
+            sweetAlert({'title': 'Comment Posted!', 'type': 'success', 'text': 'CommentID: ' + myComment.id});
+          });
+    });
+
+
+
+// Alternative Tries to get user object
 
       // var s_user = this.get('store').findRecord('user', userid);
       // var s_post = this.get('store').findRecord('post', postid);
@@ -67,30 +86,6 @@ export default Controller.extend({
       // comment.save().then(function(myComment) {
       //   sweetAlert({'title': 'Comment Posted!', 'type': 'success', 'text': 'CommentID: ' + myComment.id});
       // });
-
-      this.store.findRecord('post', postid).then(function(post) {
-    //    this.store.findRecord('user', userid).then(function(user) {
-        const comment = self.store.createRecord('comment', {
-          comment: self.get('newcomment'),
-          post: post,
-          dateSubmitted: new Date(),
-          user: username
-        });
-
-        // this.store.findRecord('user', userid).then(function(user) {
-        //   user.get('comments').then(function(comments) {
-        //     comments.addObject(comment);
-        //     user.save();
-        //   });
-        // });
-
-          comment.save().then(function(myComment) {
-            self.set('newcomment', '');
-            sweetAlert({'title': 'Comment Posted!', 'type': 'success', 'text': 'CommentID: ' + myComment.id});
-          });
-    //      });
-    });
-
 
 
     // var s_user = this.store.query('user', {
--- app/models/comment.js
+++ app/models/comment.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+//  TODO: correct referential integrity - user
   // user: DS.belongsTo('user'),
   user: DS.attr('string'),
   post: DS.belongsTo('post'),
