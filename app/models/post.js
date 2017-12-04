67abc98d267021aed00f334aa8bb0246b54f5a44
rauldoe <khoado@csu.fullerton.edu>
2017-12-03 18:53:23 -0800

    scoring

--- app/models/post.js
+++ app/models/post.js
@@ -7,7 +7,7 @@ export default DS.Model.extend({
   dateSubmitted: DS.attr('date'),
   upVote: DS.attr('number'),
   downVote: DS.attr('number'),
-  score: Ember.computed('upVote', 'downVote', function(){return this.get('upVote')-this.get('downVote');}), // calculated value (upvoters - downvoters)
+  score: Ember.computed('upVote', 'downVote', function(){return Math.max(0, this.get('upVote')-this.get('downVote'));}), // calculated value (upvoters - downvoters)
   user: DS.belongsTo('user'),
   upvoters: DS.hasMany('user', {inverse: 'favorite_posts'}), // array of user id's
   downvoters: DS.hasMany('user', {inverse: 'disliked_posts'}), // array of user id's
